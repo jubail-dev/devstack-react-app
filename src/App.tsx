a9e7278a869a8f1d600aa7aa91a5e08a627aa146
jubail-jr <hosainjubail8@gmail.com>
2026-09-11 16:02:30 +0600

    Added Footer Section

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { Hero } from './components/Hero'
 import { Nav } from './components/Nav'
 import type { TechnologiesType } from './types/types'
 import Technologies from './components/Technologies'
+import { Footer } from './components/Footer'
 
 const promiseTechnologiesData = async (): Promise<TechnologiesType[]> => {
-  const response = await fetch("../public/data.json")
+  const response = await fetch("../data.json")
   const data = await response.json()
   return data;
 }
@@ -22,6 +23,7 @@ function App() {
           <Technologies promiseTechnologies = {promiseTechnologiesData()} ></Technologies>
 
       </Suspense>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,84 @@
+import FooterLogo from "../assets/logo-text.png"
+
+export function Footer() {
+    
+    return (
+        <>
+            <footer className="container mx-auto py-20">
+
+                <div className="flex flex-col gap-10">
+
+
+                    <div className="flex justify-between">
+
+                       <div className="space-y-4">
+                        <img src={FooterLogo} alt="footer-logo" />
+                        <p className="text-[#64748B] :">Curated tools, technologies, and resources for developers building <br /> modern software.</p>
+                        <div>
+                            <ul className="flex justify-b 
+                            items-center gap-4">
+                                <li><a href="https://github.com/jubail-dev">GitHub</a></li>
+                                <li><a href="#">Twitter</a></li>
+                                <li><a href="#">LinkedIn</a></li>
+                            </ul>
+                        </div>
+                       </div>
+
+                       <div>
+                        <h2 className="text-slate-900 font-bold text-md mb-3">PRODUCT</h2>
+                        <div>
+                            <ul className="flex flex-col justify-between items-start gap-3 text-gray-400">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Technologies</a></li>
+                                <li><a href="#">Projects</a></li>
+                            </ul>
+                        </div>
+                       </div>
+
+                       <div>
+                        <h2 className="text-slate-900 font-bold text-md mb-3">COMPANY</h2>
+                        <div>
+                            <ul className="flex flex-col justify-between items-start gap-3 text-gray-400">
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">Careers</a></li>
+                            </ul>
+                        </div>
+                       </div>
+
+                       <div>
+                        <h2 className="text-slate-900 font-bold text-md mb-3">LEGAL</h2>
+                        <div>
+                            <ul className="text-gray-400 flex flex-col justify-between items-start gap-3">
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Terms of Service</a></li>
+                            </ul>
+                        </div>
+                       </div>
+                    </div>
+
+
+                     <div className="divider"></div>
+
+                        
+                    <div className="flex justify-between ">
+
+                        <div>
+                            <p className="text-[#94A3B8] text-md"> &copy; 2026 Dev Stack. All rights reserved.</p>
+                        </div>
+
+                        <div>
+                            <ul className="flex gap-2 text-[#94A3B8] text-md">
+                                <li><a href="#">Privacy</a></li>
+                                <li><a href="#">Terms</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </footer>
+        </>
+    )
+}
